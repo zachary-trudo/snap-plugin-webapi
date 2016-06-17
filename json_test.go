ad0b241742f129a5c7be34db1407bba83225d7cc
Jose G Aguirre <jgaguirr@jgaguirr-mac01.jf.intel.com>
2016-06-16 17:51:47 -0700

    Updating json unit test to account for error in ReadFile function

--- json_test.go
+++ json_test.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-   "encoding/json"
-    "io/ioutil"
-    "testing"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"testing"
 )
 
 func TestJSON(t *testing.T) {
-    buf, _ := ioutil.ReadFile("plugins.json")
-    s:= string(buf)
-    if isJSON(s){
-       //nothing here since pass by default
-    } else{
-        t.Fail()
-    }
-    
+	buf, err := ioutil.ReadFile("plugins.json")
+	if err != nil {
+		fmt.Println(err)
+
+	}
+	s := string(buf)
+	if !isJSON(s) {
+		t.Fail()
+	}
+
 }
 
-func isJSON( str string ) bool {
-     var temp interface{}
-     return json.Unmarshal([]byte(str), &temp) == nil
- }
-
+func isJSON(str string) bool {
+	var temp interface{}
+	return json.Unmarshal([]byte(str), &temp) == nil
+}
